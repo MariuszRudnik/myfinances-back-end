bad0f0a0188f5b636591d2cb2f0cb3d7671ad45f
MariuszRudnik <86014393+MariuszRudnik@users.noreply.github.com>
2022-06-30 19:15:27 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,14 @@ import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import { AuthModule } from './auth/auth.module';
-import {orm} from "./utils/orm";
 import { ExpensecategoryModule } from './expensecategory/expensecategory.module';
 import { ListOfWalletsModule } from './list-of-wallets/list-of-wallets.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import {orm} from './utils/orm'
 
 @Module({
   imports: [
-      TypeOrmModule.forRoot(orm),
+      TypeOrmModule.forRoot( orm  ),
       UserModule,
       AuthModule,
       ExpensecategoryModule,
